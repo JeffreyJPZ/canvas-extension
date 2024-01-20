6ff8a60c9f821505cb75b49b2dc87a044d292ab0
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-01-20 12:36:30 -0800

    [MOD] Added canvas logo and scrape button to popup

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from '@assets/img/logo.svg';
+import logo from '@assets/img/canvas-logo.svg';
 
 export default function Popup(): JSX.Element {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
+    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-white-800">
+      <header className="flex flex-col items-center justify-center text-black">
         <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
+        <p>Canvas Extension</p>
       </header>
+
+      <div className="flex flex-col items-center justify-center text-black">
+        Press the button below to scrape your courses
+      </div>
+
+        <button className="bg-red-500 outline-black text-black">
+            Scrape
+        </button>
     </div>
   );
 }
